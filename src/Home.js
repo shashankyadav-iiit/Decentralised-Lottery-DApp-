d99a747ac52e8ba8cbe9e46b7862447f0d685f9a
shashankyadav-iiitv <shashankyadav.iiitv@gmail.com>
2024-10-30 12:16:36 +0530

    console showing wallet address

--- src/Home.js
+++ src/Home.js
@@ -10,23 +10,31 @@ function Home() {
 
     useEffect(() => {
         const loadBlockchainData = async () => {
-            if (typeof window.ethereum !== 'undefined') {
+            if(typeof window.ethereum !== 'undefined'){
                 const provider = new ethers.providers.Web3Provider(window.ethereum);
-                try {
+                await provider.send("eth_requestAccounts", []);
+                try{
                     const signer = provider.getSigner();
-                    const address = await signer.getAddress(); // This should be valid.
-
+                    const address = await signer.getAddress();
                     console.log(address);
-                    // setCurrentAccount(address);
-                } catch (err) {
-                    console.error(err);
+                    setCurrentAccount(address);
+                    window.ethereum.on('accountsChanged', (accounts)=>{
+                        setCurrentAccount(accounts[0]);
+                        console.log(currentAccount);
+                    })
+                }catch(err){
+                    console.error(err)
                 }
-            } else {
-                alert('Please install metamask to use this application');
+            }else{
+                alert('Please install meta mask to use this application')
             }
         };
+
+        const contract = async () => {
+            const provider = new ethers.providers.Web3Provider(window.ethereum);
+        }
         loadBlockchainData();
-    }, [])
+    }, [currentAccount]);
 }
 
 export default Home;
